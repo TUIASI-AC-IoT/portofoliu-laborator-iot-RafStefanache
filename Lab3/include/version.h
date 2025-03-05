8f3ba00a80cc177545b25a53352b7d1e29d7195a
Stefanache Rafael <139012715+RafStefanache@users.noreply.github.com>
2025-03-05 19:29:02 +0200

    Add files via upload

---
+++ Lab3/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "5"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.5 - 2025-03-05 19:18:16.497248"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.5"
+#endif
